af06aa9e2553ed038eb449b64c7bb14a3c189a4b
mprive0 <monarcprive@gmail.com>
2026-04-24 16:42:12 -0700

    main

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MonarcPrive from "./MonarcPrive-Complete";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MonarcPrive />
+    <App />
   </React.StrictMode>
 );
